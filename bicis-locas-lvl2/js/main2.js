9318dceaef717badd551d882762df32a05c1829e
VeroRamirez <veronica12.rp@gmail.com>
2016-11-04 16:34:59 -0600

    Validaciones con jQuery 3

--- bicis-locas-lvl2/js/main2.js
+++ bicis-locas-lvl2/js/main2.js
@@ -23,63 +23,51 @@ function validaForm(){
 };
 
 function validaNombre() {
+        $('.name-container span').each( function(){$(this).remove(); })
         var texto = $('#name').val();
         if( texto.trim() == "" ){
-            $('.name-container span').each(function(){$(this).remove();})
             var span = $('<span class="error" >Campo Vacio</span>');
             $(".name-container").append(span);
             
         }else if(!patron.test(texto)){
-            $('.name-container span').each(function(){$(this).remove();})
             var span = $('<span class="error" >Texto invalido</span>');
             $(".name-container").append(span);
             
-        }else{
-            $('.name-container span').each(function(){$(this).remove();})
         }
 }
 
 function validaApellido() {
+        $('.lastname-container span').each( function(){$(this).remove(); })
         var texto = $('#lastname').val();
         if( texto.trim() == ""){
-            $('.lastname-container span').each(function(){$(this).remove();})
             var span = $('<span class="error" >Campo Vacio</span>');
             $(".lastname-container").append(span);
         }else if( !patron.test(texto) ){
-            $('.lastname-container span').each(function(){$(this).remove();})
             var span = $('<span class="error" >Texto invalido</span>');
             $(".lastname-container").append(span);
-        }else{
-            $('.lastname-container span').each(function(){$(this).remove();})
         }
 }
 
 function validaMail() {
+        $('.email-container span').each( function(){ $(this).remove(); })
         var texto = $('#input-email').val();
         if(texto.trim()==""){
-            $('.email-container span').each(function(){$(this).remove();})
             var span = $('<span class="error" >Campo Vacio</span>');
             $(".email-container").append(span);
         }else if(!patronEmail.test(texto)){
-            $('.email-container span').each(function(){$(this).remove();})
             var span = $('<span class="error" >Email invalido</span>');
             $(".email-container").append(span);
-        }else{
-            $('.email-container span').each(function(){$(this).remove();})
         }
     }
 function validaPassword() {
+        $('#input-password').parent().find('span').each( function() { $(this).remove(); });
         var texto = $('#input-password').val();
         if ( texto.trim() == "" ) {
-           $('#input-password').parent().find('span').each( function() { $(this).remove(); });
             var span = $('<span class="error" >Campo Vacio</span>');
             $('#input-password').parent().append(span);
         }else if( texto.length<6 ) {
-            $('#input-password').parent().find('span').each( function() { $(this).remove(); });
-            var span = $('<span class="error" >Password debe tener al menos 5 caracteres</span>');
+            var span = $('<span class="error" >Password debe tener al menos 6 caracteres</span>');
             $('#input-password').parent().append(span);
-        }else{
-            $('#input-password').parent().find('span').each( function() { $(this).remove(); });
         }
     }
 
@@ -88,9 +76,7 @@ function validaCombo() {
          if($("select").val()=="0"){
             var span = $('<span class="error" >Elige una opcion valida</span>');
             $('select').parent().append(span);
-         }else{
-             $('select').parent().find('span').each( function() { $(this).remove(); });
-        }
+         }
     }
 
 
